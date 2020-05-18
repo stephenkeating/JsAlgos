09e7fb340b4c195a53b0577c385a2a325a2e7813
Stephen Keating <stephen.keating@gmail.com>
2020-05-18 11:42:42 -0400

    pivot helper work

--- quickSort.js
+++ quickSort.js
@@ -1,1 +1,17 @@
 // quick sort algo
+
+
+// pivot helper
+
+let pivot = (arr) => {
+  let e1 = arr[0]
+  let idx = 0
+  for (let i = 1; i < arr.length; i++) {
+    let e2 = arr[i]
+    if (e1 > e2) {
+      idx++
+       
+    }
+  }
+  return idx
+}
